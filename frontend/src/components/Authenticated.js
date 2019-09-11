d74ff5dd0e243a4d7fcf6eae3359b2cacc8607a7
maniart <mani.art@gmail.com>
2019-09-11 09:45:32 -0400

    WIP trying a frontend solution for domain whitelisting

--- frontend/src/components/Authenticated.js
+++ frontend/src/components/Authenticated.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { withRouter} from 'react-router-dom'
 
 /* 1st party */
-import { LOCAL_STORAGE_AUTH_KEY, BASE_API_URL } from '../utils'
+import { LOCAL_STORAGE_AUTH_KEY, BASE_API_URL, AUTHORIZED_DOMAIN} from '../utils'
 
 export default withRouter(function Authenticated({ history }) {
   
@@ -11,16 +11,24 @@ export default withRouter(function Authenticated({ history }) {
     if (window.location.search.length && window.location.search.indexOf('id_token') >= 0) {
       let authParms = window.location.search.split('')
       authParms = authParms.splice(1).join('')
+      // here
       fetch(`${BASE_API_URL}/auth/google/callback?${authParms}`)
       .then(response => response.json())
       .then(parsedResponse => {
-        window.localStorage && window.localStorage.setItem(LOCAL_STORAGE_AUTH_KEY, JSON.stringify(parsedResponse))
-        history.push('/')
+        const  { user: { email } } = parsedResponse 
+        const domain = email.split('@')[1]
+        if (domain.includes(AUTHORIZED_DOMAIN)) {
+          window.localStorage.setItem(LOCAL_STORAGE_AUTH_KEY, JSON.stringify(parsedResponse))
+        } else {
+          window.localStorage.clear()
+          alert('Not an authorized domain. sorry')
+        }
       })
       .catch(e => console.log('Failed - ', e))
     } else {
       console.log('Redirecting home. Why are you even here?')
     }
+    history.push('/')
   }
 
   checkAuthParams()
