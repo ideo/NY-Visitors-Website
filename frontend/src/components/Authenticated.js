d11e2486dc7196cf3e38852f47e800295be09f19
maniart <mani.art@gmail.com>
2019-09-11 10:39:49 -0400

    Fix oopsie with asynx code and redirect

--- frontend/src/components/Authenticated.js
+++ frontend/src/components/Authenticated.js
@@ -11,24 +11,18 @@ export default withRouter(function Authenticated({ history }) {
     if (window.location.search.length && window.location.search.indexOf('id_token') >= 0) {
       let authParms = window.location.search.split('')
       authParms = authParms.splice(1).join('')
-      // here
+
       fetch(`${BASE_API_URL}/auth/google/callback?${authParms}`)
-      .then(response => response.json())
-      .then(parsedResponse => {
-        const  { user: { email } } = parsedResponse 
-        const domain = email.split('@')[1]
-        if (domain.includes(AUTHORIZED_DOMAIN)) {
-          window.localStorage.setItem(LOCAL_STORAGE_AUTH_KEY, JSON.stringify(parsedResponse))
-        } else {
-          window.localStorage.clear()
-          alert('Not an authorized domain. sorry')
-        }
-      })
-      .catch(e => console.log('Failed - ', e))
+        .then(response => response.json())
+        .then(parsedResponse => {
+          window.localStorage && window.localStorage.setItem(LOCAL_STORAGE_AUTH_KEY, JSON.stringify(parsedResponse))
+          history.push('/')
+        })
+        .catch(e => console.log('Failed - ', e))
     } else {
       console.log('Redirecting home. Why are you even here?')
+      history.push('/')
     }
-    history.push('/')
   }
 
   checkAuthParams()
