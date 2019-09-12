f4ebe5014812a10ab196f9c796e0b4399824430b
maniart <mani.art@gmail.com>
2019-09-12 16:35:17 -0400

    Preliminary implementation of programmatic grid alignment for mobile

--- frontend/src/components/EssentialInfo.js
+++ frontend/src/components/EssentialInfo.js
@@ -1,12 +1,28 @@
 /* 3rd party */
-import React from 'react'
+import React, { createRef } from 'react'
 
 /* 1st party */
 import Address from './Address'
 import Network from './Network'
 import Contact from './Contact'
+import { getVerticalGridAlignmentDiscrepancy, getOffsetTop } from '../utils'
+
+const wrapperElRef = createRef()
 
 export default function EssentialInfo({ data }) {
+  function getStyle() {
+    let discrepancy = 0
+    let currentMarginTop = 0
+    if (wrapperElRef && wrapperElRef.current) {
+      discrepancy = getVerticalGridAlignmentDiscrepancy(wrapperElRef.current)
+      currentMarginTop =  wrapperElRef.current.style.marginTop
+    }
+    if (discrepancy !== 0) {
+      return { marginTop: `${discrepancy}px` }
+    }
+    return {marginTop: currentMarginTop}
+  }
+
   const address = {
     line1: data['address-line-1'],
     line2: data['address-line-2'],
@@ -20,7 +36,10 @@ export default function EssentialInfo({ data }) {
     phone: data['phone']
   }
   return (
-    <div className="_wrapper-info outline">
+    <div  
+      ref={wrapperElRef}
+      className="_wrapper-info outline"
+      style={getStyle()}>
       <Address data={address} />
       <Network data={network} />
       <Contact data={contact} />  
--- frontend/src/components/WelcomeSection.js
+++ frontend/src/components/WelcomeSection.js
@@ -8,13 +8,13 @@ export default function WelcomeSection({ data }) {
   
   return (
     <section className="_section-welcome flex flex-row justify-center w-100">
-      <div className="_inner-welcome flex w-70 flex-column">
+      <div className="_inner-welcome flex w-80 w-70-ns flex-column">
         
         <h1 className="serif fw5 f3 w-100 ma0 lh-solid">
           Welcome to <span className="ttu">IDEO</span>
         </h1>
         
-        <h2 className="serif fw9 f-5 ttu mb0 mt3 lh-solid">New York</h2>
+        <h2 className="serif fw9 f1 f-headline-ns mb0 mt1 mt3-ns lh-solid">New York</h2>
 
         <EssentialInfo data={data} />
         
