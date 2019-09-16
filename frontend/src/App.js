d098d0589ea04e1dd4519bc438d67cb858ea8f5e
maniart <mani.art@gmail.com>
2019-09-16 12:03:45 -0400

    WIP animations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 /* 3rd party */
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import anime from 'animejs'
-import Rellax from 'react-rellax'
 
 /* 1st party */
 import GlobalHeader from './components/GlobalHeader'
@@ -13,30 +11,8 @@ import PlacesSection from './components/PlacesSection'
 import StudioSection from './components/StudioSection'
 import Authenticated from './components/Authenticated'
 import { hydrate, ENDPOINTS } from './utils'
-import PolkadotTexture from './styles/images/polkadot-texture.jpg'
-import Sign from './styles/images/sign.png'
-import Sandy from './styles/images/sandy.png'
-import Tom from './styles/images/tom.png'
-import Tower from './styles/images/tower.png'
-import Bridge from './styles/images/bridge.png'
 
 const Root = () => {
-  
-
-  anime({
-    targets: '#_path-blob-purple',
-    d: [
-      { value: ['M162.5,-151.6C203.3,-121.6,224.1,-60.8,217.5,-6.6C210.9,47.6,176.9,95.2,136.1,128.2C95.2,161.2,47.6,179.6,2.2,177.4C-43.1,175.1,-86.3,152.3,-135.3,119.3C-184.3,86.3,-239.1,43.1,-237.1,2C-235.1,-39.1,-176.3,-78.3,-127.3,-108.3C-78.3,-138.3,-39.1,-159.1,10.8,-170C60.8,-180.8,121.6,-181.6,162.5,-151.6Z'] },
-      { value: ['M73.3,-51.5C107.5,-39.1,156.2,-19.6,172.5,16.3C188.8,52.1,172.5,104.2,138.3,119.7C104.2,135.2,52.1,114.1,19.6,94.5C-13,75,-25.9,56.9,-49.4,41.4C-72.9,25.9,-107,13,-102.6,4.4C-98.2,-4.2,-55.5,-8.5,-32,-20.8C-8.5,-33.2,-4.2,-53.6,7.7,-61.2C19.6,-68.9,39.1,-63.8,73.3,-51.5Z'] }
-    ],
-    easing: 'easeOutBack',
-    duration: 6000,
-    loop: true,
-    autoplay: true,
-    delay: 400,
-    endDelay: 0,
-    direction: 'alternate'
-  })
 
   // hydrate `faq` and `info` items from endpoint
   const [faqs, setfaqs] = useState([])
@@ -54,7 +30,6 @@ const Root = () => {
 
   return (
     <>
-
       <GlobalHeader />
       <WelcomeSection data={info} />
       <FaqSection data={faqs} />
--- frontend/src/components/FaqSection.js
+++ frontend/src/components/FaqSection.js
@@ -1,7 +1,9 @@
 /* 3rd party */
 import React from 'react'
 import Rellax from 'react-rellax'
+import anime from 'animejs'
 
+/* 1st party */
 import PolkadotTexture from '../styles/images/polkadot-texture.jpg'
 import Sign from '../styles/images/sign.png'
 import Sandy from '../styles/images/sandy.png'
@@ -9,7 +11,26 @@ import Tom from '../styles/images/tom.png'
 import Tower from '../styles/images/tower.png'
 import Bridge from '../styles/images/bridge.png'
 
+function animate() {
+  anime({
+    targets: '#_path-blob-purple',
+    d: [
+      { value: ['M162.5,-151.6C203.3,-121.6,224.1,-60.8,217.5,-6.6C210.9,47.6,176.9,95.2,136.1,128.2C95.2,161.2,47.6,179.6,2.2,177.4C-43.1,175.1,-86.3,152.3,-135.3,119.3C-184.3,86.3,-239.1,43.1,-237.1,2C-235.1,-39.1,-176.3,-78.3,-127.3,-108.3C-78.3,-138.3,-39.1,-159.1,10.8,-170C60.8,-180.8,121.6,-181.6,162.5,-151.6Z'] },
+      { value: ['M73.3,-51.5C107.5,-39.1,156.2,-19.6,172.5,16.3C188.8,52.1,172.5,104.2,138.3,119.7C104.2,135.2,52.1,114.1,19.6,94.5C-13,75,-25.9,56.9,-49.4,41.4C-72.9,25.9,-107,13,-102.6,4.4C-98.2,-4.2,-55.5,-8.5,-32,-20.8C-8.5,-33.2,-4.2,-53.6,7.7,-61.2C19.6,-68.9,39.1,-63.8,73.3,-51.5Z'] }
+    ],
+    easing: 'easeOutBack',
+    duration: 6000,
+    loop: true,
+    autoplay: true,
+    delay: 400,
+    endDelay: 0,
+    direction: 'alternate'
+  })
+}
+
 export default function FaqSection({ data }) { 
+  animate()
+  
   return (
     <section className="_section-get-here flex flex-column w-100 items-center">        
       
@@ -34,16 +55,18 @@ export default function FaqSection({ data }) {
       </Rellax>
 
       <Rellax className="_blob-purple" as="div" speed={4} >
-      <svg
-        width="600"
-        height="600"
-        className="_inner-blob-purple"
-        viewBox="0 0 600 600"
-        xmlns="http://www.w3.org/2000/svg">
-        <g transform="translate(300,300)">
-          <path id="_path-blob-purple" d="M149,-106.3C193.3,-104.7,229.7,-52.3,208.4,-21.2C187.2,9.9,108.5,19.8,64.1,33.5C19.8,47.1,9.9,64.6,-7.3,71.9C-24.5,79.2,-49,76.4,-96.9,62.7C-144.7,49,-215.8,24.5,-220.1,-4.2C-224.3,-33,-161.7,-66,-113.8,-67.7C-66,-69.3,-33,-39.7,9.7,-49.3C52.3,-59,104.7,-108,149,-106.3Z" fill="#1f008e" />
-        </g>
-      </svg>
+        <svg
+          width="600"
+          height="600"
+          className="_inner-blob-purple"
+          viewBox="0 0 600 600"
+          xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(300,300)">
+            <path
+              id="_path-blob-purple"
+              d="M149,-106.3C193.3,-104.7,229.7,-52.3,208.4,-21.2C187.2,9.9,108.5,19.8,64.1,33.5C19.8,47.1,9.9,64.6,-7.3,71.9C-24.5,79.2,-49,76.4,-96.9,62.7C-144.7,49,-215.8,24.5,-220.1,-4.2C-224.3,-33,-161.7,-66,-113.8,-67.7C-66,-69.3,-33,-39.7,9.7,-49.3C52.3,-59,104.7,-108,149,-106.3Z" fill="#1f008e" />
+          </g>
+        </svg>
       </Rellax>
 
       <div className="_inner-get-here outline w-80 w-70-ns">
